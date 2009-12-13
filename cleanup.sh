497c91e804b4574715c9552bd1e060dbd75806a9
Gajo Petrovic <gajo01@gmail.com>
2009-12-13 11:13:13 +0100

    added two useful scripts for testing batch files and did some cleaning up in the scanner

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+rm -f *.bat.output
+rm -f *.bat.error
---
+++ test_batch_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+FILES="*.bat"
+if [ ${#FILES} -lt 1 ]; then
+    echo -en '\E[40;31m'"[ERROR]"
+    tput sgr0
+    echo -en '  '
+    echo "there are no *.bat files"
+    exit
+fi
+if [ ! -s ./batch2bash ]; then
+    echo -en '\E[40;31m'"[ERROR]"
+    tput sgr0
+    echo -en '  '
+    echo "batch2bash must exist and be in the same directory"
+    exit
+fi
+for i in $FILES
+do
+    echo -n "Parsing file: $i"
+    j=${#i}
+    while [ $j -lt 70 ]; do
+        echo -n " "
+        let j=$j+1
+    done
+    ./batch2bash < $i > $i.output 2> $i.error
+    if [ -s $i.error ]; then
+        echo -en '\E[40;31m'"[ERROR]"
+        tput sgr0
+        echo
+    else
+        echo -en '\E[40;32m'"[OK]"
+        tput sgr0
+        echo
+    fi
+done
